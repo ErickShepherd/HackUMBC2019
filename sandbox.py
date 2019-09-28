f99bb192c778dc83ae25ed3e7191fc5929db79de
Erick Shepherd <erickeshepherd@gmail.com>
2019-09-28 15:53:16 -0400

    Added a list of Python packages and brief summaries of each.

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+# Third party imports.
+import requests
+
+# Constant definitions.
+API_ENDPOINT_BASE_URL = "http://catalog.data.gov/api/3/"
+
+"""
+
+https://api.census.gov/data/
+
+    <year>/
+    timeseries/
+
+"""
+
+
+response = requests.get(url = API_ENDPOINT_BASE_URL)
+data     = response.json()
